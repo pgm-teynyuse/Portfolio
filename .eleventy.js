0b52d3662ef98b2c8c82ec4c34177f8d5aed37f2
«pgm-teynyuse» <teynur.yuseinov@student.arteveldehs.be>
2023-05-12 19:31:56 +0200

    New Repo

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = (eleventyConfig) => {
+      eleventyConfig.addPassthroughCopy({
+            'input/assets/styles/dist/': 'styles/',
+            'input/assets/images/': 'img/',
+            'input/assets/fonts/': 'fonts/',
+            'input/assets/scripts/': 'scripts/',
+            'input/assets/icons/': 'icons/',
+});
+
+
+const getCurrentYear = () => {
+      return new Date().getFullYear();
+}
+
+eleventyConfig.addShortcode('currentYear', getCurrentYear);
+
+return {
+            dir: {
+            input: './input/pages/',
+            output: 'output',
+            includes: '../_partials',
+            layouts: '../_layouts',
+            data: '../_data',
+      }
+}
+}
---
+++ output/scripts/welcome.js
@@ -0,0 +1,60 @@
+const toRotate = [
+   "Hey, ik ben Teynur!",
+   "Ik ben een Full-Stack Ontwikkelaar.",
+   "Ik hou van Design!",
+];
+
+const TxtType = function(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+   };
+
+   TxtType.prototype.tick = function() {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+      var that = this;
+      var delta = 200 - Math.random() * 100;
+
+      if (this.isDeleting) { delta /= 2; }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+      }
+
+      setTimeout(function() {
+      that.tick();
+      }, delta);
+   };
+
+window.onload = function() {
+   const elements = document.getElementsByClassName('typewrite');
+   for (let i = 0; i < elements.length; i++) {
+      const period = elements[i].getAttribute('data-period');
+      new TxtType(elements[i], toRotate, period);
+   }
+
+   // INJECT CSS
+   const css = document.createElement("style");
+   css.type = "text/css";
+   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+   document.body.appendChild(css);
+}
